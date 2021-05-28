e97c226e12a66696645f62bd2ba29918cd9f5859
Daniel Gafni <danielgafni16@gmail.com>
2021-05-28 22:24:36 +0300

    fix

--- src/make_df_example.py
+++ src/make_df_example.py
@@ -2,34 +2,40 @@ import joblib
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler  # feature_range=(-1,1)
 
-from features import features
+from src import DATA_DIR, TRACK1_DIR
+from src.features import features
 
 
-def make_water_state_encoder(base_path="track_1/", postfix=""):
-    wdf = pd.read_csv(base_path + "reference_water_codes" + postfix + ".csv")
+def make_water_state_encoder(base_path=TRACK1_DIR, postfix=""):
+    wdf = pd.read_csv(TRACK1_DIR / ("reference_water_codes" + postfix + ".csv"))
     labels = wdf["water_code"].values
     le = LabelEncoder()
     le.fit(labels)
-    joblib.dump(le, "dict_water_codes.joblib")
+    joblib.dump(le, DATA_DIR / "dict_water_codes.joblib")
     return len(list(le.classes_))
 
 
 def apply_water_state_encoder(df):
-    le = joblib.load("dict_water_codes.joblib")
+    le = joblib.load(DATA_DIR / "dict_water_codes.joblib")
     # make dict from label encoder
     # ref code -> int code
     rev_mapping = dict(zip(range(len(le.classes_)), le.classes_))
     n = len(list(le.classes_))
+
     # make new columns
     for i in range(n):
         df["water_code_" + str(i)] = (
-            df["water_code"].str.split(",").apply(lambda s: str(rev_mapping[i]) in s).astype(int)
+            df["water_code"]
+            .fillna("")
+            .str.split(",")
+            .apply(lambda s: str(rev_mapping[i]) in s if s else -1)
+            .astype(int)
         )
     return df
 
 
-def make_hydro_df(base_path="track_1/", postfix=""):
-    df = pd.read_csv(base_path + "hydro_1day" + postfix + ".csv")
+def make_hydro_df(base_path=TRACK1_DIR, postfix=""):
+    df = pd.read_csv(base_path / ("hydro_1day" + postfix + ".csv"))
     df = apply_water_state_encoder(df)
 
     # split features
@@ -53,12 +59,12 @@ def make_hydro_df(base_path="track_1/", postfix=""):
     # numerical features
     num_scaler = MinMaxScaler(feature_range=(-1, 1))
     df[num_feats] = num_scaler.fit_transform(df[num_feats])
-    joblib.dump(num_scaler, "num_scaler.joblib")
+    joblib.dump(num_scaler, DATA_DIR / "num_scaler.joblib")
 
     # categorical features
     for c in cat_feats:
         le = LabelEncoder()
         df[c] = le.fit_transform(df[c])
-        joblib.dump(le, c + "_le.joblib")
+        joblib.dump(le, DATA_DIR / (c + "_le.joblib"))
 
     return df[num_feats + cat_feats + target_feats], [num_feats, cat_feats, target_feats]
