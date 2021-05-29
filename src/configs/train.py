2b207e244f0a6ad8f616839109252c5e3d2d1e1b
Daniel Gafni <49863538+danielgafni@users.noreply.github.com>
2021-05-30 02:47:34 +0300

    fix-hyperparams (#23)
    * fix-hyperparams

--- src/configs/train.py
+++ src/configs/train.py
@@ -30,11 +30,12 @@ class TrainBiTransConfig:
     log_graph: bool = False
     version: Optional[str] = None
     gpus: int = 1
+    gradient_clip_val: float = 100.0
     batch_size: int = 128
     num_workers: int = 16
     alpha: float = 0.25
-    gamma: float = 2
-    lr: float = 3e-4
+    gamma: float = 2.5
+    lr: float = 1e-3
     weight_decay: float = 1e-3
-    max_epochs: int = 20
+    max_epochs: int = 200
     patience: int = 20
